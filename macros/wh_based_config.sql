2b2a6f65eaaf2281a19f09dd6e95055e790f2e11
Sanny Prawira <sanny.prawira@fishtownanalytics.com>
2025-10-27 03:57:06 +0000

    ff

--- macros/wh_based_config.sql
+++
@@ -1,9 +0,0 @@
-{%- macro wh_based_config_incremental(wh_size=none) %}
-
-    {% set runtime_value = var('full_refresh_warehouse', none) %}
-
-    {% if runtime_value is not none and runtime_value != 'dummy_wh' %}
-        use warehouse {{ runtime_value }}
-    {% endif %}    
-
-{% endmacro -%}
