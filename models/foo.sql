15857eb2d2cce2bc4ac0036cd180dc4e34fb3c21
Sanny Prawira <sanny.prawira@dbtlabs.com>
2025-12-12 11:57:51 +0000

    com

--- models/foo.sql
+++ models/foo.sql
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
+
 select 1 as id
-=======
-<<<<<<< HEAD
-select 1 as banana
-=======
-select 1 as potato
->>>>>>> 2791fd086d91be0ef774c7b3aac3ac7f711e8fea
->>>>>>> 8461769df03c1b4ce4a40153075c354cdb284802
